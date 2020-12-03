b8f221f3a7258b49135a105672e6cc9aa1a9858a
ewelina-wierzbicka <ewierzbicka3@gmail.com>
2020-12-03 17:30:22 +0100

    Create Gatsby and Strapi projects

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ba2542ac5cf5c78a76f2022506f90bd3'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '72e68613-0e85-4f94-99be-d30860988cc4'
+};
