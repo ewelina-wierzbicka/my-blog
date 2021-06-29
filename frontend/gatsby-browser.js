63cfeac32a26ee332c18a17f2a3f6f8ff93f1792
ewelina-wierzbicka <ewierzbicka3@gmail.com>
2021-06-29 20:23:36 +0200

    frontend updates

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { GraphQLClient, ClientContext } from "graphql-hooks"
 
-export const client = new GraphQLClient({
+let client;
+export const onClientEntry = () => {
+client = new GraphQLClient({
   url: `${process.env.API_URL}/graphql`,
-})
+});
+}
 
-const wrapRootElement = ({ element }) => (
+export const wrapRootElement = ({ element }) => (
   <ClientContext.Provider value={client}>{element}</ClientContext.Provider>
-)
-
-export default wrapRootElement
+);
