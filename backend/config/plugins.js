5221f63a4ea4217e8853a4236c84e172388d9560
ewelina-wierzbicka <ewierzbicka3@gmail.com>
2021-07-03 17:22:40 +0200

    install Cloudinary

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -12,4 +12,16 @@ module.exports = ({ env }) => ({
         connectionTimeout: 1,
       },
     },
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
   });
