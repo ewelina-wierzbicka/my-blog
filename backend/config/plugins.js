798c9ec844a4ca3b7832df66b45ade56f6f03c8e
ewelina-wierzbicka <ewierzbicka3@gmail.com>
2023-06-02 20:07:30 +0200

    revert config changes

---
+++ backend/config/plugins.js
@@ -0,0 +1,27 @@
+module.exports = ({ env }) => ({
+    email: {
+      provider: 'smtp',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        secure: false,
+        username: env('SMTP_USERNAME'),
+        password: env('SMTP_PASSWORD'),
+        rejectUnauthorized: true,
+        requireTLS: false,
+        connectionTimeout: 1,
+      },
+    },
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+  });
